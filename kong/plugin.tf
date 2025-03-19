023b02f300917d189a49ae2a5f6190fe89f1a30e
guyzsarun <sarun.nunt@gmail.com>
2025-03-19 09:06:21 +0700

    add plugin

---
+++ kong/plugin.tf
@@ -0,0 +1,15 @@
+resource "kong-gateway_plugin_prometheus" "prometheus" {
+  config = {
+    bandwidth_metrics       = true
+    latency_metrics         = true
+    per_consumer            = false
+    status_code_metrics     = true
+    upstream_health_metrics = true
+  }
+
+  enabled       = true
+  instance_name            = "prometheus-metrics"
+  protocols = [
+    "https","https"
+  ]
+}
