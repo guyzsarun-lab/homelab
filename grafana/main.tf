57f17e946e74405f5d34a1f8264ca79841584429
guyzsarun <sarun.nunt@gmail.com>
2025-07-16 21:39:24 +0700

    move dashboard

--- grafana/main.tf
+++ grafana/main.tf
@@ -5,17 +5,3 @@ resource "grafana_organization" "homelab" {
     "guyzsarun@proxmox.local"
   ]
 }
-
-resource "grafana_folder" "folder" {
-  org_id = grafana_organization.homelab.org_id
-
-  title = each.key
-
-  for_each = toset([
-    "Networking",
-    "Hardware",
-    "Public",
-    "Finance",
-    "Loki"
-  ])
-}
